2d2a01b4956b87ccef539b0ee476c479af40ca0a
Dave Senciall <dave.senciall@dfo-mpo.gc.ca>
2017-10-13 13:50:14 -0230

    updated com 8 pulsed utility

--- utilities_code/simple_nmea_sender_com8_pulsed.py
+++ utilities_code/simple_nmea_sender_com8_pulsed.py
@@ -13,19 +13,19 @@ import msvcrt # built-in module
 import sys
 
 class DataGen2(object):
-    def __init__(self, FileName,init=50):
+	def __init__(self, FileName,init=50):
 		self.data = self.init = init
 		self.FileName = FileName
 		self.f = open(self.FileName,"r")
 		print "File opened"
 
-    def next(self):
-        line=self.f.readline()
-        return line
+	def next(self):
+		line=self.f.readline()
+		return line
 
-    def close_infile(self):
-        print "closing file"
-        self.f.close()
+	def close_infile(self):
+		print "closing file"
+		self.f.close()
 
 	def rewind(self):
 		self.f.seek(0)
@@ -43,7 +43,7 @@ def on_send_timer(datagen,ser) :
 
 def kbfunc():
 
-    return msvcrt.getch() if msvcrt.kbhit() else '0'
+	return msvcrt.getch() if msvcrt.kbhit() else '0'
 
 def main() :
 	if len(sys.argv) != 2:
@@ -92,16 +92,21 @@ def main() :
 		if kk  == 'q':
 			break
 		if kk == 'f':
-			Nseconds = Nseconds - 0.005
-			if Nseconds < 0.00001: 
-				Nseconds = 0.00001
-				sys.stdout.write("\a") # plays a beep to say your at limit
 
+
+			if Nseconds < 0.00001: 
+				Nseconds = 0.0
+				sys.stdout.write("\a") # plays a beep to say your at limit
+			else:
+				Nseconds = Nseconds - 0.005
+			print Nseconds
+			
 		if kk == 's':
 			Nseconds = Nseconds + 0.005
-
-			
-		time.sleep(Nseconds)
+			print Nseconds
+	
+		if Nseconds > 0:	
+			time.sleep(Nseconds)
 		if not paused:
 			x = on_send_timer(datagen,ser)
 			if not silent:
@@ -109,11 +114,11 @@ def main() :
 			if x =='' and loop:
 				datagen.rewind()
 		
-        
+
 	datagen.close_infile()
 
 	ser.close()
 	print 'bye - port closed'
 
 if __name__ == '__main__':
-        main()
+		main()
